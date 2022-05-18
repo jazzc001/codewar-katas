048768266597c2e27ed5e00898c37b72356a76a5
jazzc001 <jazzcheung212@gmail.com>
2022-05-18 19:59:22 +0100

    finished directionsReduction

--- directionsReduction.js
+++ directionsReduction.js
@@ -1,30 +1,44 @@
 const directionsReduction = (arr) => {
-    // const countNS = 0
-    // const countEW = 0
 
-    // arr.forEach((i) => {
-    //     if (arr[i] == 'NORTH') {
-    //         countNS ++
-    //     } else if (arr[i] == 'SOUTH') {
-    //         countNS --
-    //     } else if (arr[i] == 'EAST') {
-    //         countEW ++
-    //     } else if (arr[i] == 'WEST') {
-    //         countEW --
-    //     }
-    // })
+    //logic for direction checking
+    const opposites = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+    let result = arr.reduce((acc, cur) => (opposites[acc.slice(-1)] === cur ? acc.pop() : acc.push(cur), acc), [])
 
-    // const result = [];
-    const count = {}
-    arr.forEach((i) => {
-        count[i] ? count[i]++ : count[i] = 1 ;
-    })
-    
-    if (count.NORTH === count.SOUTH && count.WEST === count.EAST) {
+    //checking unique values in array
+    const unique = [...new Set(arr)]
 
+    //checking 2 arrays are equal
+    function arrayEquals(a, b) {
+        return Array.isArray(a) &&
+          Array.isArray(b) &&
+          a.length === b.length &&
+          a.every((val, index) => val === b[index]);
+      }
+
+
+    if (arrayEquals(unique, arr)) {
+        return arr
+    } else { 
+        return result
+        
     }
+    
+    return []
+
+
+
+
+    // const count = {}
+    // arr.forEach((i) => {
+    //     count[i] ? count[i]++ : count[i] = 1 ;
+    // })
+    // const direction = []
+    // if (count.NORTH === count.SOUTH && count.WEST === count.EAST) {
+
+    // }
 }
 
-console.log(directionsReduction(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))
+// console.log(directionsReduction(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))
+// console.log(directionsReduction(["NORTH", "WEST", "SOUTH", "EAST"]))
 
 module.exports = { directionsReduction }
--- directionsReduction.test.js
+++ directionsReduction.test.js
@@ -2,6 +2,12 @@ const { directionsReduction } = require('./directionsReduction')
 
 describe('Directions', () => {
     test('Minimize the direction ', () => {
-        expect(directionsReduction(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])).toBe(["WEST"]);
+        expect(directionsReduction(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])).toEqual(["WEST"]);
+    })
+    test('Minimize the direction ', () => {
+        expect(directionsReduction(["NORTH", "WEST", "SOUTH", "EAST"])).toEqual(["NORTH", "WEST", "SOUTH", "EAST"]);
+    })
+    test('Minimize the direction ', () => {
+        expect(directionsReduction(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])).toEqual([]);
     })
 })
