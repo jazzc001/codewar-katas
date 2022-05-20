3c1e3c89945fd4cda55e559f2b811d54d72659be
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 22:26:40 +0100

    finsihed the logic and the test

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -1,17 +1,17 @@
+// O(nlogn) time, O(1)space
+
 const nonConstructibleChange = (coins) => {
 
-    //potential nonConstructibleCominations
-    let fullCombinations = []
-    for (let i = coins[0]; i < coins[coins.length-1]; i++) {
-        fullCombinations.push(i)
-    }
     
-    //get the potential nonConstructible
-    let potentialNonConstructible = fullCombinations.filter((self) => {
-        return coins.indexOf(self) === -1;
-    })
-    console.log(potentialNonConstructible)
-    return 1;
+    coins.sort((a,b) => a-b); //sorting is O(nlogn) time, 
+    
+    let changes = 0;
+    for (const coin of coins) {
+        if (coin > changes + 1) return changes +1;
+        changes += coin;
+    }
+
+    return changes + 1;
 }
 const input = [5, 7, 1, 1, 2, 3, 22];
 const expected = 20;
