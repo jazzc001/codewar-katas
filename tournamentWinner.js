dfb11dd46aa3bed2f939d1229cfe06a23421ab6d
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 03:20:24 +0100

    added tournamentwinner basic logic which will return a scoreboard

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -1,5 +1,24 @@
 const tournamentWinner = (competition, result) => {
-
+  let compIdx = 0;
+  let scoreBoard = {}
+  while (compIdx < competition.length) {
+    if (result === 0) { 
+        scoreBoard[competition[compIdx][1]] ? scoreBoard[competition[compIdx][1]] += 3 : scoreBoard[competition[compIdx][1]] = 3;
+        compIdx++;
+    } else if (result === 1) {
+        scoreBoard[competition[compIdx][0]] ? scoreBoard[competition[compIdx][0] += 3 : scoreBoard[competition[compIdx][0] = 3;
+        compIdx++;
+    }
+  }
+  return scoreBoard;
 }
 
+competition = [
+    ["HTML", "C#"],
+    ["C#", "Python"],
+    ["Python", "HTML"]
+  ]
+result = [0, 0, 1]
+
+console.log(tournamentWinner(competition, result))
 module.exports = {tournamentWinner}
