5069ae16241acdaa67e7483596214266a66ed9aa
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 22:41:54 +0100

    added test

--- threeNumberSum.test.js
+++ threeNumberSum.test.js
@@ -2,6 +2,12 @@ const { threeNumberSum } = require('./threeNumberSum')
 
 describe('threeNumberSum', () => {
     test('should', () => {
-        
+        array = [12, 3, 1, 2, -6, 5, -8, 6]
+        target = 0
+        expected = [[-8, 2, 6],
+        [-8, 3, 5],
+        [-6, 1, 5],
+        ]
+        expect(threeNumberSum(array, target)).toEqual(expected)
     })
 })
