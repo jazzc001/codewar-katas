8c8f41e4f9b678ecf5d35c08ede064d7bbe6feba
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 14:46:29 +0100

    set up testing and js file for nonConstructibleChange

---
+++ nonConstructibleChange.js
@@ -0,0 +1,5 @@
+const nonConstructibleChange = () => {
+    
+}
+
+module.exports = {nonConstructibleChange}
---
+++ nonConstructibleChange.test.js
@@ -0,0 +1,7 @@
+const { nonConstructibleChange } = require('./nonConstructibleChange')
+
+describe('nonConstructibleChange', () => {
+    test('nonConstructibleChange', ()=> {
+        
+    })
+})
