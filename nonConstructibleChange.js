fa40e6ae06d59f97b0121f3de91725252d7d361f
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 15:17:18 +0100

    tried to use the unique combination to return the nonConstructible

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -1,14 +1,29 @@
 const nonConstructibleChange = (coins) => {
+
+    // to get all combinations
     const allcombinations = [];
     for (let i = 0; i < coins.length; i++) {
         for (let j = 1; j < coins.length; j++) {
             allcombinations.push(coins[i]+coins[j])
         }
     }
-    console.log(allcombinations)
+
+    // to get the unique combinations and sorted them out in ascending order
+    // const uniqueCombinations = allcombinations.filter((value, index, self) => {self.indexOf(value) === index});
+    const uniqueCombinations = [...new Set(allcombinations)].sort((a, b) => a-b);
+
+    //find the earliest nonConstructible combinations
+    let index = 0;
+    while (index < allcombinations.length) {
+        let nonConstructible = allcombinations[index] + 1
+        if(allcombinations[index] != nonConstructible) return nonConstructible;
+        index++;
+        console.log(nonConstructible)
+    }
+    
     return 1;
 }
 const input = [5, 7, 1, 1, 2, 3, 22];
 const expected = 20;
-nonConstructibleChange(input)
+console.log(nonConstructibleChange(input))
 module.exports = {nonConstructibleChange}
