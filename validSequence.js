8848aea0deda386b31525d424ae9aea9fa606014
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 02:08:17 +0100

    solved valid sequence with edge cases

--- validSequence.js
+++ validSequence.js
@@ -1,29 +1,39 @@
 const validSequence = (array, sequence) => {
     
     //logic
-    let result = [];
-    for (let i = 0; i < sequence.length; i++) {
-        array.forEach((e) => {
-            if (e == sequence[i]) {
-                result.push(e);
-            }
-        })
+    // let result = [];
+    // for (let i = 0; i < sequence.length; i++) {
+    //     array.forEach((e) => {
+    //         if (e === sequence[i]) {
+    //             return result.push(e);
+    //         }
+    //     })
+    // }
+
+    let arrIdx = 0;
+    let seqIdx = 0;
+    while (arrIdx < array.length && seqIdx < sequence.length) {
+        if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+        arrIdx++;
     }
-    console.log(result)
+    return seqIdx === sequence.length
+
+
+    // console.log(result)
     
-    //check 2 array are equal
-    const arrayEquals = (a, b) =>  {
-        return Array.isArray(a) &&
-          Array.isArray(b) &&
-          a.length === b.length &&
-          a.every((val, index) => val === b[index]);
-      }
+    // //check 2 array are equal
+    // const arrayEquals = (a, b) =>  {
+    //     return Array.isArray(a) &&
+    //       Array.isArray(b) &&
+    //       a.length === b.length &&
+    //       a.every((val, index) => val === b[index]);
+    //   }
     
-    return arrayEquals(result, sequence);
+    // return arrayEquals(result, sequence);
 }
 
-array = [5, 1, 22, 25, 6, -1, 8, 10];
-sequence = [1, 6, -1, 10];
+array = [1, 1, 1, 1, 1];
+sequence = [1,1, 1];
 console.log(validSequence(array, sequence))
 
 
