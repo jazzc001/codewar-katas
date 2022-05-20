3f0bd869d0ff3ae322f264170857384a4c610f65
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 03:41:42 +0100

    finished the test with tournament winner

--- tournamentWinner.js
+++ tournamentWinner.js
@@ -2,15 +2,22 @@ const tournamentWinner = (competition, result) => {
   let compIdx = 0;
   let scoreBoard = {}
   while (compIdx < competition.length) {
-    if (result === 0) { 
-        scoreBoard[competition[compIdx][1]] ? scoreBoard[competition[compIdx][1]] += 3 : scoreBoard[competition[compIdx][1]] = 3;
-        compIdx++;
-    } else if (result === 1) {
-        scoreBoard[competition[compIdx][0]] ? scoreBoard[competition[compIdx][0] += 3 : scoreBoard[competition[compIdx][0] = 3;
-        compIdx++;
-    }
+      result.forEach((e) => {
+          if (e === 0) { 
+              scoreBoard[competition[compIdx][1]] ? scoreBoard[competition[compIdx][1]] += 3 : scoreBoard[competition[compIdx][1]] = 3;
+              compIdx++;
+          } else if (e === 1) {
+              scoreBoard[competition[compIdx][0]] ? scoreBoard[competition[compIdx][0]] += 3 : scoreBoard[competition[compIdx][0]] = 3;
+              compIdx++;
+          }
+      })
   }
-  return scoreBoard;
+
+  const values = Object.keys(scoreBoard).map((key)=> {return scoreBoard[key]});
+  const keys = Object.keys(scoreBoard).map((key)=> { return key})
+  const max = Math.max.apply(null, values);
+  const indexMax =  values.indexOf(max)
+  return keys[indexMax]
 }
 
 competition = [
