a3a8fe5a305966e40ab3a03ec079caf477688871
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 14:56:22 +0100

    added basic logic for all combinations in nonConstructible change

--- nonConstructibleChange.js
+++ nonConstructibleChange.js
@@ -1,5 +1,14 @@
 const nonConstructibleChange = (coins) => {
-
+    const allcombinations = [];
+    for (let i = 0; i < coins.length; i++) {
+        for (let j = 1; j < coins.length; j++) {
+            allcombinations.push(coins[i]+coins[j])
+        }
+    }
+    console.log(allcombinations)
+    return 1;
 }
-
+const input = [5, 7, 1, 1, 2, 3, 22];
+const expected = 20;
+nonConstructibleChange(input)
 module.exports = {nonConstructibleChange}
