72967eca3a978a1d1f01c2e161e5a929c0ff45a3
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 02:55:08 +0100

    added test that deal with ascending order of output

--- sortedSquaredArray.js
+++ sortedSquaredArray.js
@@ -1,5 +1,7 @@
-const sortedSquaredArray = () => {
-
+const sortedSquaredArray = (array) => {
+    return array.map((e) => e*e).sort((a, b) => a-b)
 }
+// array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(sortedSquaredArray(array))
 
 module.exports = { sortedSquaredArray }
--- sortedSquaredArray.test.js
+++ sortedSquaredArray.test.js
@@ -2,7 +2,11 @@ const { sortedSquaredArray } = require('./sortedSquaredArray');
 
 describe('sortedSquaredArray', () =>{
     test('should', () => {
-        array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        array = [1, 2, 3, 5, 6, 8, 9]
         expect(sortedSquaredArray(array)).toEqual([1, 4, 9, 25, 36, 64, 81])
+    })
+    test('edge cases', () => {
+        array = [10, 5, 0, 25, 10]
+        expect(sortedSquaredArray(array)).toEqual([0 ,25, 25, 100, 100])
     })
 })
