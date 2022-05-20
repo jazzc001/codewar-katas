be1bd8787f7368389bd006b5441d3bcdbb3c4a3a
jazzc001 <jazzcheung212@gmail.com>
2022-05-20 01:47:02 +0100

    initial valideSequence logic

--- validSequence.js
+++ validSequence.js
@@ -1,7 +1,18 @@
-
-
 const validSequenc = (array, sequence) => {
-
+    let result = [];
+    for (let i = 0; i < sequence.length; i++) {
+        array.forEach((e) => {
+            if (e == sequence[i]) {
+                result.push(e);
+            }
+        })
+    }
+    console.log(result)
 }
+
+array = [5, 1, 22, 25, 6, -1, 8, 10];
+sequence = [1, 6, -1, 10];
+validSequenc(array, sequence)
+
 
 module.exports = { validSequenc }
