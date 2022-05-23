0ce17520dcdd0befbd04d3792ed7cdebe4a3a75a
jazzc001 <jazzcheung212@gmail.com>
2022-05-23 16:55:40 +0100

    finished writing the logic with test passed

--- monotonicArray.js
+++ monotonicArray.js
@@ -1,5 +1,16 @@
-const monotonicArray = () => {
+const monotonicArray = (array) => {
+    let resultIdx = 0
+    for (let i = 0; i< array.length; i++) {
+        let arrayiAbs = Math.abs(array[i])
+        let arrayiplus1Abs = Math.abs(array[i+1])
+       if (arrayiAbs === arrayiplus1Abs) resultIdx++;
 
+    }
+    return resultIdx > 0
 }
+
+const array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+const expected = true;
+console.log(monotonicArray(array))
 
 module.exports = { monotonicArray }
--- monotonicArray.test.js
+++ monotonicArray.test.js
@@ -2,6 +2,8 @@ const { monotonicArray } = require('./monotonicArray')
 
 describe('monotonicArray', () => {
     test('should', () => {
-        
+        const array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+        const expected = true;
+        expect(monotonicArray(array)).toEqual(expected);
     })
 })
