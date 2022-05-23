7c9fa32443760c496619e53e2b4584d24ec1594f
jazzc001 <jazzcheung212@gmail.com>
2022-05-23 14:33:57 +0100

    find the greatest value in array

--- waterContainer.js
+++ waterContainer.js
@@ -1,11 +1,9 @@
 const waterContainer = (height) => {
-    const array1 = [...new Set(height.sort((a, b) => a-b))];
-    const highest = array1[array1.length - 1]
-    console.log(height.indexOf(highest))
-    const secondHighest = array1[array1.length - 2]
-    console.log(height.indexOf(secondHighest))
-    const len = Math.abs(height.indexOf(highest) - height.indexOf(secondHighest))
-    return secondHighest * len
+    let largest = 0
+    height.forEach((i) => {
+        if ( i > largest ) largest = i;
+    })
+    console.log(largest)
 }
 
 height = [1,8,6,2,5,4,8,3,7]
