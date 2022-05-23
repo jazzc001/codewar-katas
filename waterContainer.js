55411416866f78fa2daeda38b76105880a4c34de
jazzc001 <jazzcheung212@gmail.com>
2022-05-23 14:58:56 +0100

    findihsed the logic

--- waterContainer.js
+++ waterContainer.js
@@ -1,9 +1,20 @@
 const waterContainer = (height) => {
     let largest = 0
+    let largestIndex = 0
     height.forEach((i) => {
-        if ( i > largest ) largest = i;
+        if ( i > largest ) {
+            largest = i
+            largestIndex = height.indexOf(largest)
+        };
     })
-    console.log(largest)
+    // find the distance of the largest wiht others and corresponding values, store it in a hashmap
+    let distance = []  //{value in height: distance}
+    height.forEach((i) => {
+        let len = Math.abs(height.indexOf(i) - largestIndex)
+        distance.push(len* i)
+        
+    })
+   return distance.sort((a, b) => a-b)[distance.length -1]
 }
 
 height = [1,8,6,2,5,4,8,3,7]
