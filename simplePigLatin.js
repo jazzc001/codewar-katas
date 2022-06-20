5b843d2d7074d4650e6088e8a5273fae0ad5d623
jazzc001 <jazzcheung212@gmail.com>
2022-06-20 22:32:47 +0100

    modified the logic to consider symbols

--- simplePigLatin.js
+++ simplePigLatin.js
@@ -2,7 +2,7 @@ const simplePigLatin = (str) => {
     const newStr =  str.split(' ')
     console.log(newStr[0][0])
     for (let i = 0; i < newStr.length;i++){
-        if (newStr[i] === '!' || '?') {
+        if (newStr[i] === '!' || newStr[i] === '?') {
             newStr[i] = newStr[i]
         } else {
             newStr[i] = newStr[i].substring(1)+ newStr[i][0] + 'ay'
@@ -12,6 +12,6 @@ const simplePigLatin = (str) => {
     return newStr.join(' ')
 };
 
-console.log(simplePigLatin('Pig latin is cool'))
+console.log(simplePigLatin('Pig latin is cool ?'))
 
 module.exports = { simplePigLatin };
