0fece0979c545fbe5466908f5400438a88ea89a3
jazzc001 <jazzcheung212@gmail.com>
2022-06-20 23:14:28 +0100

    optimized the logic of password validation

--- regexPasswordValidation.js
+++ regexPasswordValidation.js
@@ -1,10 +1,6 @@
 const validate = (password) => {
     const symbols = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/
-    return password.length>=6 && 
-    /[a-z]/.test(password) && 
-    /[A-Z]/.test(password) && 
-    /[0-9]/.test(password) && 
-    !symbols.test(password)
+    return /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\w{6,}$/.test(password)
 };
 
 console.log(validate('djI38D55'));
