5501c9f5b7969881068222e792dc082550fc8058
jazzc001 <jazzcheung212@gmail.com>
2022-07-12 22:46:25 +0100

    solved the problem

--- growthOfAPopulation.js
+++ growthOfAPopulation.js
@@ -1,5 +1,15 @@
 const nbYear = (p0, percent, aug, p) => {
+    let currentPopulation = [p0];
+    let y = 0
+    for (let i = 0; currentPopulation[i] < p; i++) {
+        currentPopulation.push(currentPopulation[i]*(1+(percent/100)) + aug);
+        y++;
+    }
 
+    return y;
+   
 }
+
+console.log(nbYear(1500, 5, 100, 5000))
 
 module.exports = {nbYear};
