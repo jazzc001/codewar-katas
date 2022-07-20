305d90cbfcd4ffffad59b5e5a816a110791d34bd
jazzc001 <jazzcheung212@gmail.com>
2022-07-20 13:26:21 +0100

    finished the logic

--- trappingRainwater.js
+++ trappingRainwater.js
@@ -1,27 +1,28 @@
 const trap = (heights) => {
     // current water = Min(MaxL, MaxR) - cH
 
-    let maxL = 0;
-    let maxR = 0;
+    
     let cH = 0;
     let accWater = 0;
 
     for (let p1 = 0; p1 = heights.length; p1++) {
+        let maxL = 0;
+        let maxR = 0;
         for (let l = p1; l > 0; l--) {
-            for (let r = p1; r< heights.length; r++) {
-                maxL = Math.max(maxL, l);
-                maxR = Math.max(maxR, r);
-                cH = heights[p1];
-                let currentWater = Math.min(maxL, maxR) - cH;
-                currentWater < 0 ? currentWater = 0 : currentWater = currentWater;
-                accWater += currentWater;
-                maxL = 0;
-                maxR = 0;
-                cH = 0;
-
-            }
+            maxL = Math.max(maxL, l);
         }
+
+        for (let r = p1; r< heights.length; r++) {
+            maxR = Math.max(maxR, r);
+        }
+        cH = heights[p1];
+        let currentWater = Math.min(maxL, maxR) - cH;
+        currentWater >= 0 ? accWater += currentWater : currentWater = 0;  
+        
     }
+    return accWater
 }
+
+console.log(trap([0,1,0,2]))
 
 module.exports = {trap}
