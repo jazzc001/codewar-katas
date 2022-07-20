290eb2d10c33ac1c121ec3d455d97c0d6790cff5
jazzc001 <jazzcheung212@gmail.com>
2022-07-20 13:10:17 +0100

    current height

--- trappingRainwater.js
+++ trappingRainwater.js
@@ -1,5 +1,27 @@
-const {trap} = (heights) => {
-     
+const trap = (heights) => {
+    // current water = Min(MaxL, MaxR) - cH
+
+    let maxL = 0;
+    let maxR = 0;
+    let cH = 0;
+    let accWater = 0;
+
+    for (let p1 = 0; p1 = heights.length; p1++) {
+        for (let l = p1; l > 0; l--) {
+            for (let r = p1; r< heights.length; r++) {
+                maxL = Math.max(maxL, l);
+                maxR = Math.max(maxR, r);
+                cH = heights[p1];
+                let currentWater = Math.min(maxL, maxR) - cH;
+                currentWater < 0 ? currentWater = 0 : currentWater = currentWater;
+                accWater += currentWater;
+                maxL = 0;
+                maxR = 0;
+                cH = 0;
+
+            }
+        }
+    }
 }
 
 module.exports = {trap}
--- trappingRainwater.test.js
+++ trappingRainwater.test.js
@@ -2,6 +2,6 @@ const { trap } = require('./trappingRainwater')
 
 describe('Rainwater', () => {
     test('should', () => {
-        
+        expect(trap([0,1,0,2,1,0,3,1,0,1,2])).toBe(8)
     })
 })
