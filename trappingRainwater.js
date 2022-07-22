290f59892d9efcb758bcddb42c96e63ecdf57ca3
jazzc001 <jazzcheung212@gmail.com>
2022-07-22 15:25:52 +0100

    figuring solution 2

--- trappingRainwater.js
+++ trappingRainwater.js
@@ -1,27 +1,29 @@
 const trap = (heights) => {
     // current water = Min(MaxL, MaxR) - cH
 
-    
-    let cH = 0;
+    //Solution 1
     let accWater = 0;
 
     for (let p1 = 0; p1 = heights.length; p1++) {
         let maxL = 0;
         let maxR = 0;
-        for (let l = p1; l > 0; l--) {
+        for (let l = p1; l >= 0; l--) {
             maxL = Math.max(maxL, l);
         }
 
         for (let r = p1; r< heights.length; r++) {
             maxR = Math.max(maxR, r);
         }
-        cH = heights[p1];
-        let currentWater = Math.min(maxL, maxR) - cH;
+        let currentWater = Math.min(maxL, maxR) - heights[p1];
         currentWater >= 0 ? accWater += currentWater : currentWater = 0;  
         
     }
     return accWater
-}
+
+    //Solution 2
+
+    
+};
 
 /*
 time: O(n^2) time
